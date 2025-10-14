81c66b07c6909cf6c2db85c8f5d24911432ddfee
Tomas Gutierrez <tgut03@gmail.com>
2025-10-14 13:08:01 -0500

    solution for problem 4

--- p4.py
+++ p4.py
@@ -1,8 +1,7 @@
 """
-Find the largest palindrome made from the product of two 3-digit numbers.
+Find the largest palindrome created from the product of two 3-digit numbers.
 """
 
-factors = []
 palindromes = []
 for num1 in range(100, 1000):
     for num2 in range(100, 1000):
@@ -14,18 +13,13 @@ for num1 in range(100, 1000):
                 is_palindrome = False
         
         if is_palindrome:
-            facts = [num1, num2]
             product = int(product)
-            factors.append(facts)
             palindromes.append(product)
 
-largest_palindrome_factors = factors[0]
 largest_palindrome = palindromes[0]
 for i, p in enumerate(palindromes):
     if p > largest_palindrome:
-        largest_palindrome_factors = factors[i]
         largest_palindrome = p
     else:
         continue
-print(f"factors = {largest_palindrome_factors}")
-print(f"largest palindrome = {largest_palindrome}")
+print(largest_palindrome)
