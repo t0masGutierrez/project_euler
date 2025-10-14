99576992a29ddcb95a0fc82216840cf87566975d
Tomas Gutierrez <tgut03@gmail.com>
2025-10-14 13:00:11 -0500

    solution for problem 0

--- p0.py
+++ p0.py
@@ -1,9 +1,9 @@
 """
-Among the first 784 thousand square numbers, what is the sum of all the odd squares?
+What is the sum of all the odd squares for the first 784 thousand square numbers?
 """
 
 sum = 0
 for i in range(754001):
     if i % 2 != 0:
         sum += i ** 2
-print(f"sum = {sum}")
+print(sum)
