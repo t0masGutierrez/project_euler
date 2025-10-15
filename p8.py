e4cd334637dede367ba7b188d4141faa6c9b6d7c
Tomas Gutierrez <tgut03@gmail.com>
2025-10-15 13:51:01 -0500

    solution for problem 8

--- p8.py
+++ p8.py
@@ -1,5 +1,5 @@
 """
-Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+Find the greatest product of thirteen adjacent digits in the 1000-digit number.
 """
 
 digits1 = "73167176531330624919225119674426574742355349194934"
