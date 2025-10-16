8fa01f3e94095dcd47599c610c40e847e495740f
Tomas Gutierrez <tgut03@gmail.com>
2025-10-15 20:40:16 -0500

    solution for problem 5

--- p5.py
+++ p5.py
@@ -2,16 +2,16 @@
 Find the smallest number divisible by all of the numbers from 1 to 20?
 """
 
-number = 1
+n = 1
 is_div = False
 while is_div == False:
     num_indiv = 0
     for i in range(1, 21):
-        remainder = number % i
+        remainder = n % i
         if remainder != 0:
             num_indiv += 1
     if num_indiv == 0:
         is_div = True
     else:
-        number += 1
-print(number)
+        n += 1
+print(n)
