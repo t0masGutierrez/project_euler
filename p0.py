accbec620054cd752c12dfc5ce00b6b38db18367
Tomas Gutierrez <tgut03@gmail.com>
2025-11-01 15:40:22 -0500

    solution for problem 0

--- p0.py
+++ p0.py
@@ -1,9 +1,27 @@
-"""
-Find the sum of all the odd squares for the first 784 thousand square numbers?
-"""
+def solution(n):
+    """
+    Find the sum of all the odd squares for the first N square numbers. 
 
-sum = 0
-for i in range(754001):
-    if i % 2 != 0:
-        sum += i ** 2
-print(sum)
+    Parameters
+    ----------
+    n : int
+        The number of odd squares
+
+    Returns
+    -------
+    sum : int
+        The sum of the odd squares
+
+    """
+    sum = 0
+    for i in range(n+1):
+        if i % 2 != 0:
+            sum += i ** 2
+    return sum
+
+def main():
+    y = solution(754000)
+    print(y)
+
+if __name__ == "__main__":
+    main()
