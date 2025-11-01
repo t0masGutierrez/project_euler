e69bea6daea6cdaccf1b2d2814926aca68ffe328
Tomas Gutierrez <tgut03@gmail.com>
2025-11-01 16:19:44 -0500

    solution for problem 3

--- p3.py
+++ p3.py
@@ -1,21 +1,35 @@
-"""
-Find the largest prime factor of 600851475143.
-"""
+def solution(N):
+    """
+    Find the largest prime factor of N.
 
-n = 600851475143
-max = int(n ** 0.5)
+    Parameters
+    ----------
+    N : int
+        The number being prime factorized
 
-prime_factors = []
-for i in range(2, max):
-    divisible = n % i == 0
-    multiple = False
+    Returns
+    -------
+    largest_prime_factor : int
+        The largest prime factor
+    """
+    MAX_PRIME_FACTOR = int(N ** 0.5)
+    prime_factors = []
+    for i in range(2, MAX_PRIME_FACTOR):
+        divisible = N % i == 0
+        multiple = False
+        for p in prime_factors:
+            if i % p == 0:
+                multiple = True
 
-    for p in prime_factors:
-        if i % p == 0:
-            multiple = True
+        if divisible and not multiple:
+            prime_factors.append(i)
 
-    if divisible and not multiple:
-        prime_factors.append(i)
+    largest_prime_factor = prime_factors[-1]
+    return largest_prime_factor
 
-largest_prime_factor = prime_factors[-1]
-print(largest_prime_factor)
+def main():
+    y = solution(600851475143)
+    print(y)
+
+if __name__ == "__main__":
+    main()
