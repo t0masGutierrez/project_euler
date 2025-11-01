13af68873714d4377fd355e83778d48b936f9c00
Tomas Gutierrez <tgut03@gmail.com>
2025-11-01 17:52:50 -0500

    solution for problem 9

--- p9.py
+++ p9.py
@@ -1,21 +1,37 @@
-"""
-There exists exactly 1 Pythagorean triplet where a + b + c = 1000.
-Find the product abc.
-"""
+def solution(N):
+    """
+    There exists exactly 1 Pythagorean triplet where a + b + c = N. Find the product abc.
 
-a = 1
-b = a + 1
-sum = 0
-while sum != 1000:
-    square = a ** 2 + b ** 2
-    c = square ** 0.5
-    sum = a + b + c
-    if sum == 1000:
-        break
-    elif sum < 1000:
-        b += 1
-    else: 
-        a += 1
-        b = a + 1
-product = int(a * b * c)
-print(product)
+    Parameters
+    ----------
+    N: int
+        The sum of the pythagorean triplet
+
+    Returns
+    -------
+    product : int
+        The product of the pythagorean triplet addends
+    """
+    a = 1
+    b = a + 1
+    sum = 0
+    while sum != N:
+        square = a ** 2 + b ** 2
+        c = square ** 0.5
+        sum = a + b + c
+        if sum == N:
+            break
+        elif sum < N:
+            b += 1
+        else: 
+            a += 1
+            b = a + 1
+    product = int(a * b * c)
+    return product
+
+def main():
+    y = solution(1000)
+    print(y)
+
+if __name__ == "__main__":
+    main()
