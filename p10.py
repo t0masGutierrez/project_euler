c7e23b97261764f8c90eceb423451a240336c046
Tomas Gutierrez <tgut03@gmail.com>
2025-11-01 17:59:58 -0500

    solution for problem 10

--- p10.py
+++ p10.py
@@ -1,18 +1,33 @@
-"""
-Find the sum of all the primes below two million.
-"""
+def solution(N):
+    """
+    Find the sum of all the primes below N.
 
-sum = 2
-for n1 in range(3, 2000000):
-    if n1 % 2 == 0:
-        continue
-    else:
-        root = int(n1 ** 0.5)
-        is_prime = True
-        for n2 in range(3, root+1, 2):
-            if n1 % n2 == 0:
-                is_prime = False
-                break
-        if is_prime:
-            sum += n1
-print(sum)
+    Parameters
+    ----------
+    N: int
+        The maximum prime number
+
+    Returns
+    -------
+    sum : int
+        The sum of the prime numbers
+    """
+    sum = 2
+    for n1 in range(3, N):
+        if n1 % 2 != 0:
+            MAX_PRIME_FACTOR = int(n1 ** 0.5)
+            is_prime = True
+            for n2 in range(3, MAX_PRIME_FACTOR+1, 2):
+                if n1 % n2 == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                sum += n1
+    return sum
+
+def main():
+    y = solution(2000000)
+    print(y)
+
+if __name__ == "__main__":
+    main()
