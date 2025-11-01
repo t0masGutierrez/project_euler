f2244d9a825895a3a99410bb775459efd0d16fd2
Tomas Gutierrez <tgut03@gmail.com>
2025-11-01 17:34:22 -0500

    solution for problem 6

--- p6.py
+++ p6.py
@@ -1,13 +1,30 @@
-"""
-Find the difference between the square of the sum and sum of the squares of the first 100 natural numbers.
-"""
+def solution(N):
+    """
+    Find the difference between the square of the sum and sum of the squares of the first N natural numbers. 
 
-sum_squares = 0
-squared_sum = 0
-for i in range(1, 101):
-    sum_squares += i ** 2
-    squared_sum += i
+    Parameters
+    ----------
+    N: int
+        The maximum natural number
 
-squared_sum **= 2
-diff = squared_sum - sum_squares
-print(diff)
+    Returns
+    -------
+    diff : int
+        The difference between square of sum and sum of squares
+    """
+    sum_squares = 0
+    squared_sum = 0
+    for i in range(1, N+1):
+        sum_squares += i ** 2
+        squared_sum += i
+
+    squared_sum **= 2
+    diff = squared_sum - sum_squares
+    return diff
+
+def main():
+    y = solution(100)
+    print(y)
+
+if __name__ == "__main__":
+    main()
