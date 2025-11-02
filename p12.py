fb772a6e3e2f34d8ddd226844c55510b6a35d45c
Tomas Gutierrez <tgut03@gmail.com>
2025-11-02 09:39:07 -0600

    solution for problem 12

---
+++ p12.py
@@ -0,0 +1,38 @@
+def solution(N):
+    """
+    Find the first triangle number to have over N divisors.
+
+    Parameters
+    ----------
+    N: int
+        The minimum number of divisors
+
+    Returns
+    -------
+    triangle_num : int
+        The value of the triangle number 
+    """
+    num_divisors = 0
+    natural_num = 1
+    while num_divisors <= N:
+        triangle_num = 0
+        for n in range(1, natural_num):
+            triangle_num += n
+        
+        num_divisors = 0
+        MAX_DIVISOR = int(triangle_num ** 0.5)
+        if MAX_DIVISOR ** 2 == triangle_num:
+            num_divisors += 1
+        
+        for n in range(1, MAX_DIVISOR):
+            if triangle_num % n == 0:
+                num_divisors += 2
+        natural_num += 1
+    return triangle_num
+
+def main():
+    y = solution(500)
+    print(y)
+
+if __name__ == "__main__":
+    main()
