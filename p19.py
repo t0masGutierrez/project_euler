e0c1210a003ca01004f5d9f1c5f9a39a30a4d3b4
Tomas Gutierrez <tgut03@gmail.com>
2025-11-30 07:33:47 -0600

    solution for problem 19

--- p19.py
+++ p19.py
@@ -2,6 +2,11 @@ def solution(N):
     """
     Find the number of Sundays on the first of the month during the Nth century.
 
+    Parameters
+    ----------
+    N : int
+        The century
+
     Returns
     -------
     num_sundays : int
@@ -14,9 +19,14 @@ def solution(N):
         leap_days = 0
         month_days = {}
         for j in range(12):
-            if j in [3, 5, 8, 10]:
+            APRIL_INDEX = 3
+            JUNE_INDEX = 5
+            SEPTEMBER_INDEX = 8
+            NOVEMBER_INDEX = 10
+            FEBRUARY_INDEX = 1
+            if j in [APRIL_INDEX, JUNE_INDEX, SEPTEMBER_INDEX, NOVEMBER_INDEX]:
                 month_days[j] = 30
-            elif j == 1:
+            elif j == FEBRUARY_INDEX:
                 if i % 4 == 0 and i % 400 != 0:
                     month_days[j] = 29
                     leap_days += 1
@@ -30,7 +40,7 @@ def solution(N):
             num_days = 7
             for m in range(month):
                 num_days += month_days[m]
-            num_days += 365 * (i - 1901) + leap_days
+            num_days += 365 * (i - start) + leap_days
             if num_days % 7 == 0:
                 num_sundays += 1
     return num_sundays
